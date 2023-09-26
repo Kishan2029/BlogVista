176c1c0ba38f34a95eac385ce8f15f21c010c18c
Kishan2029 <kishanprajapati2029@gmail.com>
2023-09-26 21:10:59 +0530

    home page made responsive

--- src/components/BlogRow.jsx
+++ src/components/BlogRow.jsx
@@ -1,12 +1,15 @@
+// "use client";
 import Image from "next/image";
 import React from "react";
 import coverImage from "../../public/images/demo1.jpeg";
 import Link from "next/link";
+import { stringConcat } from "@/util/helper";
 
 const BlogRow = ({ title, time, author, summary, blogId }) => {
+  // const width = window.innerWidth;
   return (
     // <Link href="/blog/2">
-    <div className="flex gap-x-10 cursor-pointer">
+    <div className="flex flex-col space-y-4 cursor-pointer sm:flex-row sm:space-y-0  sm:space-x-10">
       <div className="basis-2/5">
         <Link href={`/blog/${blogId}`}>
           <Image
@@ -20,12 +23,30 @@ const BlogRow = ({ title, time, author, summary, blogId }) => {
       <div className="basis-3/5">
         {/* <div className=""> */}
         <Link href={`/blog/${blogId}`}>
-          <div className="text-3xl font-medium">{title}</div>
+          <div className="text-3xl font-medium hidden lg:block">
+            {stringConcat(title, 64)}
+          </div>
+          <div className="text-2xl font-medium hidden sm:block lg:hidden">
+            {stringConcat(title, 42)}
+          </div>
+          <div className="text-2xl font-medium sm:hidden">
+            {stringConcat(title, 30)}
+          </div>
           <div className="flex gap-x-6 mt-6 mb-6">
             <div className="text-sm font-blod">{author}</div>
             <div className="text-sm font-light">{time}</div>
           </div>
-          <div className="text-sm font-light leading-loose">{summary}</div>
+          <div className="hidden sm:block">
+            <div className="text-sm font-light leading-loose hidden lg:block">
+              {stringConcat(summary, 350)}
+            </div>
+            <div className="text-sm font-light leading-loose hidden sm:block lg:hidden">
+              {stringConcat(summary, 200)}
+            </div>
+            <div className="text-sm font-light leading-loose sm:hidden">
+              {stringConcat(summary, 100)}
+            </div>
+          </div>
         </Link>
       </div>
     </div>
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,3 +1,7 @@
 export const stringSplit = (string) => {
     return string.split("\n");
 }
+
+export const stringConcat = (string, len) => {
+    return string.length > len ? string.slice(0, len) + "..." : string;
+}
